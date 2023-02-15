35b0b4b2979b5fefce38bce4089c86c70d726802
Hussein AL-hamed <90855344+HSN4@users.noreply.github.com>
2023-02-15 06:16:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 
 let img = document.querySelectorAll(".card img"),
     card = Array.from( document.querySelectorAll(".card"));
-console.log(img.length)
-console.log(card)
 img.forEach((item)=>{ item.onclick = function(){ 
     for(let i = 0 ; i < img.length ; i++){
         card[i].classList.remove("active");
